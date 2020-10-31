f2550912408e7da1ddb28b64a98ae33a1c5426ec
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-10-31 21:40:56 +0800

    fix: add optimizer

--- neural_network/ffnn_tf2.py
+++ neural_network/ffnn_tf2.py
@@ -53,6 +53,7 @@ class DNN(tf.keras.Model):
 
         self.output_layer = tf.keras.layers.Dense(units=self.num_classes)
         self.criterion = tf.losses.CategoricalCrossentropy()
+        self.optimizer = tf.optimizers.Adam(learning_rate=kwargs.get("learning_rate"))
 
     @tf.function
     def call(self, features):
