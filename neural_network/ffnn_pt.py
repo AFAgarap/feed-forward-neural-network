dccd9000dcf0b101756e2358dc8623bf730596ee
Abien Fred Agarap <abienfred.agarap@gmail.com>
2020-10-31 21:51:44 +0800

    chore: add training accuracy attribute

--- neural_network/ffnn_pt.py
+++ neural_network/ffnn_pt.py
@@ -50,6 +50,7 @@ class DNN(torch.nn.Module):
         self.optimizer = torch.optim.Adam(params=self.parameters(), lr=learning_rate)
         self.criterion = torch.nn.CrossEntropyLoss().to(self.model_device)
         self.train_loss = []
+        self.train_accuracy = []
 
     def forward(self, features: torch.Tensor) -> torch.Tensor:
         """
