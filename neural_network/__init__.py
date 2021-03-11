aa4c7b4c638974d21a78078ab118dd20f79f0e21
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-03-11 19:22:16 +0800

    docs: update license year

--- neural_network/__init__.py
+++ neural_network/__init__.py
@@ -1,5 +1,5 @@
 # Feed-Forward Neural Network
-# Copyright (C) 2017-2020 Abien Fred Agarap
+# Copyright (C) 2017-2021 Abien Fred Agarap
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
