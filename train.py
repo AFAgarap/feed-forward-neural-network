722009fe8bd00fd2df8a27ef21fd0f0a40cd480c
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-03-13 19:36:52 +0800

    add module metadata

--- train.py
+++ train.py
@@ -17,6 +17,9 @@
 """Training module for WDBC classifier"""
 from pt_datasets import load_dataset, create_dataloader
 
+__author__ = "Abien Fred Agarap"
+__version__ = "2.0.0"
+
 
 train_data, test_data = load_dataset("wdbc")
 train_loader = create_dataloader(train_data, batch_size=32, num_workers=4)
