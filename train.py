856186e156ffb52cbcdac6af232a3de46fb7d62f
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-03-13 19:34:13 +0800

    create data loader for training set

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
-from pt_datasets import load_dataset
+from pt_datasets import load_dataset, create_dataloader
 
 
 train_data, test_data = load_dataset("wdbc")
+train_loader = create_dataloader(train_data, batch_size=32, num_workers=4)
