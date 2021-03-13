37f9d1ca3f7082190d9967e5959ebe6f224e5b3c
Abien Fred Agarap <abienfred.agarap@gmail.com>
2021-03-13 19:36:34 +0800

    add module docstring

--- train.py
+++ train.py
@@ -14,6 +14,7 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 # from pt_datasets import load_dataset, create_dataloader
+"""Training module for WDBC classifier"""
 from pt_datasets import load_dataset, create_dataloader
 
 
